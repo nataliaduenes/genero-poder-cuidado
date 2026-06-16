1a25f82c151cbbf868001830565bae3b586bc6ee
nataliaduenes <angieduenes@unisangil.edu.co>
2026-06-16 09:54:07 -0500

    Delete pages/pages directory

--- pages/pages/2_Quien_posee_la_tierra.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("🌳 ¿Quién posee la tierra?")
-
-st.markdown("""
-
-Esta sección presenta la distribución por género de las personas propietarias o poseedoras de los predios vinculados al proyecto.
-
-""")
