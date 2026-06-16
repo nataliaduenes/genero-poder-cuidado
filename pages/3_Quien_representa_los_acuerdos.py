63ff41bdccf3d37a88b28a8a4d84856a09ed56a4
nataliaduenes <angieduenes@unisangil.edu.co>
2026-06-16 09:08:56 -0500

    Create 3_Quien_representa_los_acuerdos.py

---
+++ pages/3_Quien_representa_los_acuerdos.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("✍️ ¿Quién representa los acuerdos?")
+
+st.markdown("""
+
+La representación efectiva de los acuerdos permite aproximarse a quién ejerce la gestión cotidiana y administrativa de los procesos de conservación.
+
+""")
