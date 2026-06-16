402b4a4cc17fbda2bee86884a3c6e1153323fd0b
nataliaduenes <angieduenes@unisangil.edu.co>
2026-06-16 09:06:52 -0500

    Create 1_Contexto_territorial.py

---
+++ pages/1_Contexto_territorial.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("📍 Contexto territorial")
+
+st.markdown("""
+
+Los acuerdos de conservación analizados se ubican en:
+
+- Paya (Boyacá)
+- Nunchía (Casanare)
+- Yopal (Casanare)
+
+El interés del proyecto es comprender cómo las relaciones de género y poder influyen en las decisiones económicas y de cuidado en los hogares rurales.
+
+""")
