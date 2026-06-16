feebdee06d70f0a3b3847d530f8c28f7de8220c4
nataliaduenes <angieduenes@unisangil.edu.co>
2026-06-16 09:11:04 -0500

    Create 6_Prioridades_emergentes.py

---
+++ pages/pages/6_Prioridades_emergentes.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("🌱 Prioridades emergentes")
+
+st.markdown("""
+
+Las inversiones se agrupan en cinco grandes dimensiones:
+
+- Producción y sostenimiento predial.
+- Patrimonio y acumulación.
+- Hogar y calidad de vida.
+- Cuidado y bienestar familiar.
+- Innovación y sostenibilidad.
+
+""")
