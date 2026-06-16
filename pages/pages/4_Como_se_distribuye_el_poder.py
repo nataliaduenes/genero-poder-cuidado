6e2a077ba1ab6402c3c2805a21f02e506839950d
nataliaduenes <angieduenes@unisangil.edu.co>
2026-06-16 09:50:26 -0500

    Delete pages/pages directory

--- pages/pages/4_Como_se_distribuye_el_poder.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("⚖️ ¿Cómo se distribuye el poder?")
-
-st.markdown("""
-
-La relación entre propiedad y representación permite explorar las dinámicas de poder presentes dentro de los hogares rurales.
-
-""")
--- pages/pages/5_En_que_se_materializan_las_decisiones.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("💰 ¿En qué se materializan las decisiones?")
-
-st.markdown("""
-
-Las prioridades de inversión muestran cómo se traducen las decisiones económicas en los hogares rurales.
-
-""")
